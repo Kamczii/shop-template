cf68c0103dd50939d75274b70d7b716074dbde75
Kamczii <kamil.czepiell@gmail.com>
2020-09-12 10:49:14 +0200

    search on header

--- src/app/shared/search-products-input/search-products-input.component.ts
+++ src/app/shared/search-products-input/search-products-input.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { startWith, map } from 'rxjs/operators';
+import { startWith, map, tap } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Product } from '../models/product';
 
@@ -21,14 +21,14 @@ export class SearchProductsInputComponent implements OnInit {
     // this.filteredOptions = this.myControl.valueChanges.pipe(
     //   startWith(''),
     //   map(value => this._filter(value))
-    // );
-    this.myControl.setValue('')
-    this.filteredOptions = this.firestore.collection<Product>('products', ref => ref
-    .orderBy("name")
-    .startAt(this.myControl.value.toLowerCase())
-    .endAt(this.myControl.value.toLowerCase()+"\uf8ff")
-    .limit(10)).valueChanges();
-    this.filteredOptions.subscribe(data => console.log(data))
+    // // );
+    // this.myControl.setValue('')
+    // this.filteredOptions = this.firestore.collection<Product>('products', ref => ref
+    // .orderBy("name")
+    // .startAt(this.myControl.value.toLowerCase())
+    // .endAt(this.myControl.value.toLowerCase()+"\uf8ff")
+    // .limit(10)).valueChanges();
+    // this.filteredOptions.subscribe(data => console.log(data))
   }
 
   private _filter(value: string): string[] {
@@ -38,7 +38,10 @@ export class SearchProductsInputComponent implements OnInit {
   }
 
   search(){
-    this.filteredOptions = this.firestore.collection<Product>('products', ref => ref
+    let value = this.myControl.value;
+    if(value.length>2){
+
+      this.filteredOptions = this.firestore.collection<Product>('products', ref => ref
       .orderBy("name")
       .startAt(this.myControl.value)
       .endAt(this.myControl.value+"\uf8ff")
@@ -49,8 +52,10 @@ export class SearchProductsInputComponent implements OnInit {
             data.id = a.payload.doc.id;
             return data;
           });
-        })
+        }),
+        tap(data => console.log(data))
       );
+    }
     
   }
 }
