1c1b3ce79c377cb8cf49d923675a481432d47733
Kamczii <45385166+Kamczii@users.noreply.github.com>
2020-09-17 23:43:25 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDJhx-q9dwrklG4F0RuB-4MrgKcCMyo7ic",
-    authDomain: "shop-dfb7b.firebaseapp.com",
-    databaseURL: "https://shop-dfb7b.firebaseio.com",
-    projectId: "shop-dfb7b",
-    storageBucket: "shop-dfb7b.appspot.com",
-    messagingSenderId: "813583519069",
-    appId: "1:813583519069:web:1692acb187328744282cb4",
-    measurementId: "G-QLP2ZQFH68"
   }
 };
 
